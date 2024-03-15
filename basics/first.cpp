49f4dcbfe2d69a49a6257e86133d7691dd96c1b8
miki's code <140938256+mikis-code@users.noreply.github.com>
2024-03-15 15:19:22 +0530

    first commit

---
+++ basics/first.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream> 
+// #include is preprocessor directive
+//  using namespace std;   bad practice
+
+int main()
+{
+  
+    std::cout << "Hello World";  // scope operator(::)
+ 
+    return 0;
+}
